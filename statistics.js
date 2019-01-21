8316a8b88de3df76eafb1abda99dca14387adbdd
zhanglihua-wanglu <zhang.lihua@wanglutech.com>
2019-01-21 17:32:19 +0800

    update statistics

--- statistics.js
+++ statistics.js
@@ -1,5 +1,6 @@
 const CoinNodeObj = require('conf/coinNodeObj.js')
-const sleep = require('ko-sleep')
+//const date = require('date')
+const pu = require("promisefy-util")
 let log = console
 
 let web3Instance = new CoinNodeObj(log, 'wan');
@@ -20,25 +21,45 @@ async function statistics() {
 
   let ret =  web3.eth.blockNumber
 
+  blocksP = []
   for (let i = 0; i < ret; i++) {
-    block =  web3.eth.getBlock(i, true)
+    //block =  web3.eth.getBlock(i, true)
+    blockp = pu.promisefy(web3.eth.getBlock, [i,true], web3.eth);
+    blocksP.push(blockp)
+    //console.log("Time:", Date.now(), "blocknumber: ", i)
 
+
+  }
+  let blocks = await Promise.all(blocksP)
+  for(let i=0; i<blocks.length; i++) {
+    let block = blocks[i]
     if (addrCount[block.miner] == undefined) {
       addrCount[block.miner] = 0
     }
 
-    addrCount[block.miner]++
 
-    // block.transactions.forEach((item,index)=>{
-    //   if(slotScAddr === item.to){
-    //     let rawTx = web3.eth.getRawTransaction(item.hash)
-    //     slotSize += Buffer.byteLength(rawTx, 'utf8')
-    //   }
-    //   if(rbScAddr === item.to) {
-    //     let rawTx = web3.eth.getRawTransaction(item.hash)
-    //     rbSize += Buffer.byteLength(rawTx, 'utf8')
-    //   }
-    // })
+    addrCount[block.miner]++
+    let slottxPS = []
+    let rbtxPS = []
+    block.transactions.forEach((item,index)=>{
+      if(slotScAddr === item.to){
+        let rawTx = pu.promisefy(web3.eth.getRawTransaction,[item.hash],web3.eth)
+        slottxPS.push(rawTx)
+
+      }
+      if(rbScAddr === item.to) {
+        let rawTx = pu.promisefy(web3.eth.getRawTransaction,[item.hash],web3.eth)
+        rbtxPS.push(rawTx)
+      }
+    })
+    let slotTxs = await Promise.all(slottxPS)
+    let rbTxs = await Promise.all(rbtxPS)
+    rbTxs.forEach(item=>{
+      rbSize += Buffer.byteLength(item, 'utf8')
+    })
+    slotTxs.forEach(item=>{
+      slotSize += Buffer.byteLength(item, 'utf8')
+    })
   }
 
   log.info(ret)
