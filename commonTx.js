16bef272e9fdbdb4ca0540295c8e5d2d5df7f4e8
MoLin <molin@wanchain.org>
2019-04-10 17:39:45 +0800

    update commonTx.js.

--- commonTx.js
+++ commonTx.js
@@ -6,27 +6,31 @@ let log = console
 
 let web3Instance = new CoinNodeObj(log, 'wanipc');
 let web3 = web3Instance.getClient()
-const from = "0x23Fc2eDa99667fD3df3CAa7cE7e798d94Eec06eb"
-const to = "0x435b316A70CdB8143d56B3967Aacdb6392FD6125"
+const from = "0x9cd8230d43464aE97F60BAD6DE9566a064990E55";//"0xC4F682E30aa722053C52feA538db77e2042F7980"
+const to = "0xcf696d8eea08a311780fb89b20d4f0895198a489"
 let lastBlock = 0
 main();
+
+let totalSendTx = 0;
 
 async function checkBlock() {
   let blockNumber  = await pu.promisefy(web3.eth.getBlockNumber,[], web3.eth)
   if(blockNumber != lastBlock){
     lastBlock = blockNumber
     let block = await pu.promisefy(web3.eth.getBlock,[blockNumber,true], web3.eth)
-    log.log("block ",blockNumber, "has ", block.transactions.length, " txs")
+    log.log(new Date(), ">>>>>>>>>>>>>>>> block ",blockNumber, "has ", block.transactions.length, " txs")
   }
 
 }
+
+let startTime = new Date()
 async function main() {
   while(1){
-    checkBlock()
+    //checkBlock()
     try {
       let txpoolStatus = await pu.promisefy(web3.txpool.status,[], web3.txpool)
       let pendingNumber = Number(txpoolStatus.pending)
-      log.log("pending: ", pendingNumber)
+      log.log(new Date(), "pending: ", pendingNumber)
       if(pendingNumber > 80000){
         //await pu.sleep(1000)
         continue
@@ -41,8 +45,13 @@ async function main() {
       rs.push(r)
     }
     await Promise.all(rs)
+    totalSendTx+=3000
     //await pu.sleep(1000)
-    log.log("send 3000 txs")
+    let timePass = new Date() - startTime;
+
+    timePass = timePass / 1000
+    
+    log.log(new Date(), "send 3000 txs, total:", totalSendTx, "tps: ", totalSendTx/timePass)
   }
 
   console.log("done.")
