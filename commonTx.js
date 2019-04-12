d7b52b0f52114cf3ee9ba3f509a668cc72cd34b1
MoLin <molin@wanchain.org>
2019-04-12 13:34:09 +0800

    update for nonce get.

--- commonTx.js
+++ commonTx.js
@@ -19,7 +19,7 @@ if (!true) {
 } else {
   from = "0xcf696d8eea08a311780fb89b20d4f0895198a489";//"0x9cd8230d43464aE97F60BAD6DE9566a064990E55";//"0xC4F682E30aa722053C52feA538db77e2042F7980"
   to = "0x9cd8230d43464aE97F60BAD6DE9566a064990E55"
-  privateKeyJsonString = ""
+  privateKeyJsonString = '{"type":"Buffer","data":[]}'
 }
 
 var privateKey = Buffer.from(JSON.parse(privateKeyJsonString).data);
@@ -68,10 +68,16 @@ function SignTx() {
 
 let startTime = new Date()
 let txCount = 6000
-let nonce = 0
+let nonce = null
 async function main() {
-  web3.eth.getTransactionCount(from, null, (no) => { nonce = no });
-  await pu.sleep(1000)
+  // while(!nonce) {
+  //   web3.eth.getTransactionCount(from, null, (no) => { nonce = no;console.log("nonce:", nonce); });
+  //   await pu.sleep(1000)
+  // }
+
+  nonce = await  pu.promisefy(web3.eth.getTransactionCount, [from], web3.eth);
+  console.log("nonce:", nonce)
+
   while (1) {
     //checkBlock()
     try {
