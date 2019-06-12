ae610d98bf06147b3c5b0b6f53307968f021ed6f
Mo Lin <molin@wanchain.org>
2019-06-12 20:55:42 +0800

    Update activity check tools.

---
+++ checkSlotActivity.js
@@ -0,0 +1,53 @@
+const CoinNodeObj = require('conf/coinNodeObj.js')
+const fs = require('fs')
+const pu=require('promisefy-util')
+
+
+let log = console
+
+let web3Instance = new CoinNodeObj(log, 'wanipc');
+const beginBlock = 1555
+const K=10
+const slotCount=12*K
+let lastSlotID = beginBlock-1
+
+mainLoopAsync()
+
+
+
+async function mainLoopAsync() {
+  log.info("\nMain loop begins...Wait");
+  web3 = web3Instance.getClient();
+  let info = await pu.promisefy(web3.pos.getPosInfo, [], web3.pos)
+  let curEpoch = await pu.promisefy(web3.pos.getEpochID, [], web3.pos);
+  console.log("firstEpochId: ", info.firstEpochId);
+
+  console.log('slot activity -----------------------')
+  for (let i = info.firstEpochId; i < curEpoch; i++) {
+    let act = await pu.promisefy(web3.pos.getActivity, [i], web3.pos);
+    let missEp = 0;
+    let missRp = 0;
+    for (let m=0; m<act.epActivity.length; m++) {
+      if(act.epActivity[m] == 0) {
+        missEp++;
+      }
+    }
+
+    for (let m=0; m<act.rpActivity.length; m++) {
+      if(act.rpActivity[m] == 0) {
+        missRp++;
+      }
+    }
+    
+    console.log(i, act.slActivity, missEp, missRp)
+  }
+
+  console.log('ep / rp activity -----------------------')
+
+  for (let i = info.firstEpochId; i < curEpoch; i++) {
+    let act = await pu.promisefy(web3.pos.getActivity, [i], web3.pos);
+
+  }
+
+  console.log("all finish")
+}
