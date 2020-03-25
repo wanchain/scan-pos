fac97c12ff8415ad1c387f012bd0aa8435407c0a
Zhang Lihua <zhang.lihua@wanchaintech.com>
2020-03-25 19:27:00 +0800

    update white list test

--- staking/whiteListTest.js
+++ staking/whiteListTest.js
@@ -6,8 +6,8 @@ let log = console
 let web3Instance = new CoinNodeObj(log, 'wan');
 let web3 = web3Instance.getClient()
 const assert = require('assert');
-let coinbase="0x28CA21654E3E082A0D282DAF2010352c157008E0";
-
+let owner="0x31BC508f0F6CA9384340a6B06A046baB1C91E624";
+let other="0x576713551E21b136c5B775f5Fb4081441773b2D4"
 let passwd = "wanglu"
 
 
@@ -42,7 +42,7 @@ var contractDef = web3.eth.contract(wlcDefinition);
 var wlContractAddr = "0x0000000000000000000000000000000000000264";
 var wlContract = contractDef.at(wlContractAddr);
 
-var payload = wlContract.upgradeWhiteEpochLeader.getData(2608636, 0,26)
-var tx = web3.personal.sendTransaction({from:coinbase, to:wlContractAddr, value:'0x00', data:payload, gas: 200000, gasprice:'0x' + (200000000000).toString(16)},"wanglu");
+var payload = wlContract.upgradeWhiteEpochLeader.getData(2608636, 0,20)
+var tx = web3.personal.sendTransaction({from:owner, to:wlContractAddr, value:'0x00', data:payload, gas: 200000, gasprice:'0x' + (200000000000).toString(16)},"wanglu");
 console.log("tx=" + tx)
 //------------------RUN CODE DO NOT MODIFY------------------
